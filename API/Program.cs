153b9e8200f2dc1ebd5952aa1ddafb1448336e2f
Ruby Chang <dev.rubimoon@gmail.com>
2022-07-21 11:21:17 +0900

    initialize project before publishing to heroku

---
+++ API/Program.cs
@@ -0,0 +1,102 @@
+using Application.Activities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to container
+builder.Services.AddControllers(opt =>
+              {
+                  var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                  opt.Filters.Add(new AuthorizeFilter(policy));
+              })
+                  .AddFluentValidation(config =>
+              {
+                  config.RegisterValidatorsFromAssemblyContaining<Create>();
+              });
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+// Configure the http request pipeline
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseXContentTypeOptions();
+app.UseReferrerPolicy(opt => opt.NoReferrer());
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+app.UseXfo(opt => opt.Deny());
+app.UseCsp(opt => opt
+    .BlockAllMixedContent()
+    .StyleSources(s => s.Self().CustomSources(
+        "https://fonts.googleapis.com",
+        "sha256-/epqQuRElKW1Z83z1Sg8Bs2MKi99Nrq41Z3fnS2Nrgk=",
+        "sha256-2aahydUs+he2AO0g7YZuG67RGvfE9VXGbycVgIwMnBI=",
+        "sha256-+oGcdj5BhO6SoiIGYIkPOMYi7d2h2Pp/bkJLBfYL+kk="
+    ))
+    .FontSources(s => s.Self().CustomSources(
+        "https://fonts.gstatic.com", "data:"
+    ))
+    .FormActions(s => s.Self())
+    .FrameAncestors(s => s.Self())
+    .ImageSources(s => s.Self().CustomSources(
+        "https://res.cloudinary.com",
+        "https://www.facebook.com",
+        "https://platform-lookaside.fbsbx.com",
+        "data:"
+        ))
+    .ScriptSources(s => s.Self()
+        .CustomSources(
+            "sha256-HIgflxNtM43xg36bBIUoPTUuo+CXZ319LsTVRtsZ/VU=",
+            "https://connect.facebook.net",
+            "sha256-3x3EykMfFJtFd84iFKuZG0MoGAo5XdRfl3rq3r//ydA="
+        ))
+);
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+}
+else
+{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
+}
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<ChatHub>("/chat");
+app.MapFallbackToController("Index", "Fallback");
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context, userManager);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migraiton");
+}
+
+
+await app.RunAsync();
+
